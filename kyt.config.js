c9082ece1e1a503c43b87cb7cebfe2b0bd875d54
Julian V. Modesto <julian.modesto@nytimes.com>
2017-02-13 16:54:47 -0500

    Upgrade to kyt@0.4.0

--- kyt.config.js
+++ kyt.config.js
@@ -5,18 +5,4 @@
 module.exports = {
   reactHotLoader: true,
   debug: false,
-  modifyWebpackConfig: (baseConfig, options) => {
-    const appConfig = Object.assign({}, baseConfig);
-    const babelLoader = appConfig.module.rules.find(loader => loader.loader === 'babel-loader');
-    babelLoader.options.plugins.push(
-        'babel-plugin-transform-class-properties',
-        'babel-plugin-transform-object-rest-spread');
-
-    // add babel-polyfill entry for client only
-    if (options.type === 'client') {
-      appConfig.entry.main.unshift('babel-polyfill');
-    }
-
-    return appConfig;
-  },
 };
